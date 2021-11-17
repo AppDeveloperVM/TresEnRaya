6a895853012edeb9006ad2ecb478fd4964a999ab
AppDeveloperVM <vicmr1994@gmail.com>
2021-11-17 09:08:55 +0100

    Added DataBinding to gameFragment - Layout and class

--- app/src/main/java/com/example/tresenraya/GameFragment.kt
+++ app/src/main/java/com/example/tresenraya/GameFragment.kt
@@ -6,26 +6,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.tresenraya.databinding.GameFragmentBinding
 
 class GameFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = GameFragment()
-    }
-
+    private lateinit var binding: GameFragmentBinding
     private lateinit var viewModel: GameViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.game_fragment,
+            container,
+            false
+        )
+
+        viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
+
+        binding.lifecycleOwner = viewLifecycleOwner
+
+
         return inflater.inflate(R.layout.game_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
 
 }
