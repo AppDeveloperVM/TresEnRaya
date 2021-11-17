65d62f46179e7d7f2c1257932d2f46550f080860
AppDeveloperVM <vicmr1994@gmail.com>
2021-11-17 10:12:50 +0100

    GameFragment moved, created GameViewModel

--- app/src/main/java/com/example/tresenraya/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/tresenraya/screens/game/GameFragment.kt
@@ -1,12 +1,14 @@
-package com.example.tresenraya
+package com.example.tresenraya.screens.game
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import com.example.tresenraya.R
 import com.example.tresenraya.databinding.GameFragmentBinding
 
 class GameFragment : Fragment() {
@@ -29,9 +31,20 @@ class GameFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
 
         binding.lifecycleOwner = viewLifecycleOwner
-
+        viewModel.eventGameFinished.observe(viewLifecycleOwner,
+            { hasFinished -> if (hasFinished) gameFinished() })
 
         return inflater.inflate(R.layout.game_fragment, container, false)
+    }
+
+
+    private fun gameFinished(){
+        Log.i("GameFragment", "Game has just finished")
+
+        viewModel.onGameFinishComplete()
+        val action =
+            com.example.tresenraya.GameFragmentDirections.actionGameFragmentToScoreFragment()
+
     }
 
 
---
+++ app/src/main/java/com/example/tresenraya/screens/game/GameViewModel.kt
@@ -0,0 +1,82 @@
+package com.example.tresenraya.screens.game
+
+import android.os.CountDownTimer
+import android.text.format.DateUtils
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() {
+
+    private val filas = 3
+    private val columnas = 3
+    /*private var _tablero = MutableLiveData<Array<String?>>(3,"")
+    var tablero: LiveData<Array<String?>>
+        get() = _tablero
+
+     */
+
+    private val _turn = MutableLiveData<Int>(0)
+    val turn: LiveData<Int>
+        get() = _turn
+
+    private val _score = MutableLiveData<Int>(0)
+    val score: LiveData<Int>
+        get() = _score
+
+    private val _eventGameFinished = MutableLiveData<Boolean>(false)
+    val eventGameFinished: LiveData<Boolean>
+        get() = _eventGameFinished
+
+    private val _currentTime = MutableLiveData<Long>()
+    val currentTime: LiveData<Long>
+        get() = _currentTime
+
+    private val timer: CountDownTimer
+
+    val currentTimeString: LiveData<String> = Transformations.map(currentTime) { time ->
+        DateUtils.formatElapsedTime(time)
+    }
+
+
+
+
+
+    init {
+        //resetList()
+        //en()
+
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND){
+
+            override fun onTick(millisUntilFinished: Long) {
+                _currentTime.value = millisUntilFinished / ONE_SECOND
+            }
+
+            override fun onFinish() {
+                _currentTime.value = DONE
+            }
+        }
+
+        timer.start()
+    }
+
+
+    fun onGameFinish() {
+        _eventGameFinished.value = true
+    }
+
+    fun onGameFinishComplete() {
+        _eventGameFinished.value = false
+    }
+
+
+    companion object {
+        private const val DONE = 0L
+
+        private const val ONE_SECOND = 1_000L
+
+        private const val COUNTDOWN_TIME = 60_000L
+    }
+
+}
