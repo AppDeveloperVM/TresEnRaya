44517b6a174fcbecd4bd225e269b9031203dafb9
Víctor Maree Redondo <victor.maree.redondo@copernic.alumnat.cat>
2021-12-02 10:28:59 +0100

    Cleaned mainActivity

--- app/src/main/java/com/example/tresenraya/MainActivity.kt
+++ app/src/main/java/com/example/tresenraya/MainActivity.kt
@@ -16,28 +16,6 @@ import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
-    private val filas = 3
-    private val columnas = 3
-    private val totalCasillas : Int = filas * columnas
-    private var casillasVacias : Int = totalCasillas
-    //private var tablero = mutableMapOf<Int,String>()
-    private var _tablero= Array(filas) { arrayOfNulls<String>(columnas) } //Array(filas) { Array<String>(size = columnas) }
-
-    private val players = arrayOf("Jugador", "Máquina")
-    private lateinit var turntxt: TextView // TURNO
-    private var turnNumber: Int = 0
-    private var actual_player: String = ""
-    private lateinit var symbol_player: String
-
-    private lateinit var reset: Button
-
-    private var mc_turn_end : Boolean = false
-    private var enable_py: Boolean = true
-    private val machine_turn_time : Long = 400
-
-    private var game_ended: Boolean = false
-    private var winner: String = ""
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
