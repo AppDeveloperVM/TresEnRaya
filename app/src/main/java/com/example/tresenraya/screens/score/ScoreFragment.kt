d478fb3bc199612a0ce306ed3de6bd070ecefc5b
AppDeveloperVM <vicmr1994@gmail.com>
2021-12-08 20:56:09 +0100

    little tests

--- app/src/main/java/com/example/tresenraya/screens/score/ScoreFragment.kt
+++ app/src/main/java/com/example/tresenraya/screens/score/ScoreFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -44,7 +45,9 @@ class ScoreFragment : Fragment() {
 
         viewModel.eventPlayAgain.observe(viewLifecycleOwner) { playAgain ->
             if (playAgain) {
+                //val bundle = bundleOf("score" to 22)
                 findNavController().navigate(ScoreFragmentDirections.actionScoreFragmentToGameFragment() )
+
                 viewModel.onPlayAgainComplete()
             }
         }
