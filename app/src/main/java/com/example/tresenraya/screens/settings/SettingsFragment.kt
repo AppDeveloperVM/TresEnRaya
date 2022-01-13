fa9f6723fc6cc3e5a65385e3b64a175291543297
Víctor Maree Redondo <victor.maree.redondo@copernic.alumnat.cat>
2022-01-13 18:08:57 +0100

    Update SettingsFragment.kt

--- app/src/main/java/com/example/tresenraya/screens/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/tresenraya/screens/settings/SettingsFragment.kt
@@ -1,11 +1,14 @@
 package com.example.tresenraya.screens.settings
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
 import com.example.tresenraya.R
 
-class SettingsFragment : PreferenceFragmentCompat() {
+class SettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener  {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
@@ -17,11 +20,26 @@ class SettingsFragment : PreferenceFragmentCompat() {
         */
     }
 
+    override fun onPreferenceTreeClick(preference: Preference?): Boolean {
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(requireContext())
+
+        //Log.d(TAG, "${preference?.key}")
+        return super.onPreferenceTreeClick(preference)
+    }
+
+
     override fun onResume() {
         super.onResume()
-        preferenceManager
-            .sharedPreferences
-            //.registerOnSharedPreferenceChangeListener(this)
+        preferenceManager.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onPause() {
+        preferenceManager.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
+        super.onPause()
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+
     }
 
 
